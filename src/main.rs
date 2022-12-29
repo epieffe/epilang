552ff3627f6930a01b15eda21e66b2c64b014189
Epifanio Ferrari <ferrariepifanio@gmail.com>
2022-12-29 03:23:32 +0100

    Improved interactive shell using rustyline dependency

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,16 @@
-use std::collections::HashMap;
-use std::io;
-use std::io::Write;
-
 mod expression;
 mod token;
 mod parser;
 mod tokenizer;
 mod semantics;
+mod shell;
+
+use std::collections::HashMap;
 
 use tokenizer::tokenize;
-use parser::parse;
 use parser::parse_in_scope;
-use semantics::eval;
-use semantics::Error;
 
 use token::Token;
-
-use token::Operand;
-use token::Operator;
 
 use expression::Const;
 use expression::Exp;
@@ -25,7 +18,13 @@ use expression::Exp;
 use crate::semantics::eval_expression;
 
 fn main() {
-    interactive_shell()
+    let mode = "SHELL";
+
+    if mode == "SHELL" {
+        shell::run_shell()
+    } else {
+        run_text()
+    }
 }
 
 fn run_text() {
@@ -64,101 +63,6 @@ fn run_text() {
 
 }
 
-fn interactive_shell() {
-    let mut stack: Vec<Const> = Vec::new();
-    let mut variable_scope_map: HashMap<String, usize> = HashMap::new();
-    let mut scope = 0;
-    loop {
-        // Reqest user input
-        print!("epilang> ");
-        let mut user_input = String::new();
-        let stdin = io::stdin();
-        io::stdout().flush().expect("flush failed!");
-        stdin.read_line(&mut user_input).expect("failed to read stdin");
-        // Check exit condition
-        if user_input.trim() == "exit" {
-            break
-        }
-        // Tokenize string
-        let mut tokens: Vec<Token> = match tokenize(user_input) {
-            Result::Ok(tokens) => tokens,
-            Result::Err(err) => {
-                println!("Syntax error during token parsing");
-                continue
-            }
-        };
-        if tokens.is_empty() { continue }
-
-        // We need to handle let expression separately when in interactive mode
-        let is_let: bool = match tokens.last() {
-            Option::Some(Token::Let) => true,
-            _ => false
-        };
-        if is_let {
-            match eval_let(&mut tokens, &mut stack, scope, &mut variable_scope_map) {
-                // Increment scope after evaluating let
-                Result::Ok(new_scope) => scope = new_scope,
-                Result::Err(msg) => println!("{}", msg)
-            }
-            continue;
-        }
-        // Parse tokens to exp
-        let exp: Exp = match parse_in_scope(&mut tokens, scope, &mut variable_scope_map) {
-            Result::Ok(exp) => exp,
-            Result::Err(_) => {
-                println!("Syntax error");
-                continue
-            }
-        };
-        // Evaluate expression
-        match eval_expression(exp, &mut stack) {
-            Result::Ok(Const::None) => (),
-            Result::Ok(val) => println!("{}", val),
-            Result::Err(err) => println!("Error: {}", err.msg)
-        }
-    }
-}
-
-/**
- * We need to handle let expression in a different way when
- * in interactive mode
- */
-fn eval_let(
-    tokens: &mut Vec<Token>,
-    stack: &mut Vec<Const>,
-    scope: usize,
-    variable_scope_map: &mut HashMap<String, usize>
-) -> Result<usize, String> {
-    // Pop let token
-    match tokens.pop() {
-        Option::Some(Token::Let) => (),
-        _ => panic!("First token must be a let when calling handle_let function")
-    };
-    // Pop variable token
-    let var_name = match tokens.pop() {
-        Option::Some(Token::Operand(Operand::Var(name))) => name,
-        _ => return Result::Err(String::from("SyntaxError: expected variable token"))
-    };
-
-    // Pop "=" token
-    match tokens.pop() {
-        Option::Some(Token::Operator(Operator::Assign)) => (),
-        _ => return Result::Err(String::from("SyntaxError: expected '=' token"))
-    };
-
-    let exp: Exp = match parse_in_scope(tokens, scope, variable_scope_map) {
-        Result::Ok(exp) => exp,
-        Result::Err(err) => return Result::Err(String::from("SyntaxError"))
-    };
-    let val = match eval_expression(exp, stack) {
-        Result::Ok(val) => val,
-        Result::Err(err) => return Result::Err(err.msg)
-    };
-    variable_scope_map.insert(var_name, scope);
-    stack.push(val);
-    Result::Ok(scope + 1)
-}
-
 fn const_to_string(c: &Const) -> String {
     match c {
         Const::Integer(i) => i.to_string(),
@@ -189,5 +93,3 @@ fn exp_to_string(exp: &Exp) -> String {
         Exp::IfThenElse(e, e1, e2) => format!("if {} {{ {} }} else {{ {} }}", exp_to_string(e), exp_to_string(e1), exp_to_string(e2))
     }
 }
-
-
---
+++ src/shell.rs
@@ -0,0 +1,136 @@
+use std::collections::HashMap;
+
+use rustyline::error::ReadlineError;
+use rustyline::{Editor};
+
+use crate::tokenizer::tokenize;
+use crate::parser::parse_in_scope;
+use crate::semantics::eval_expression;
+
+use crate::expression::Exp;
+use crate::expression::Const;
+use crate::expression::Var;
+
+use crate::token::Token;
+use crate::token::Operand;
+use crate::token::Operator;
+
+pub fn run_shell() {
+    let mut stack: Vec<Const> = Vec::new();
+    let mut variable_scope_map: HashMap<String, usize> = HashMap::new();
+    let mut scope = 0;
+
+    let mut rl: Editor<()> = Editor::<()>::new().expect("Error creating editor");
+    loop {
+        match rl.readline("epilang> ") {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+                handle_user_input(line, &mut stack, scope, &mut variable_scope_map)
+            },
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break
+            },
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break
+            },
+            Err(err) => {
+                println!("ReadlineError: {:?}", err);
+                break
+            }
+        };
+    }
+}
+
+/**
+ * Evaluating user input can change the actual scope.
+ * The new scope is returned.
+ */
+fn handle_user_input(
+    line: String,
+    stack: &mut Vec<Const>,
+    mut scope: usize,
+    variable_scope_map: &mut HashMap<String, usize>
+) -> usize {
+    // Tokenize string
+    let mut tokens: Vec<Token> = match tokenize(line) {
+        Result::Ok(tokens) => tokens,
+        Result::Err(err) => {
+            println!("Syntax error during token parsing");
+            return scope
+        }
+    };
+    if tokens.is_empty() { return scope }
+
+    // We need to handle let expression separately when in interactive mode
+    let is_let: bool = match tokens.last() {
+        Option::Some(Token::Let) => true,
+        _ => false
+    };
+    if is_let {
+        match eval_let(&mut tokens, stack, scope, variable_scope_map) {
+            // Increment scope after evaluating let
+            Result::Ok(new_scope) => scope = new_scope,
+            Result::Err(msg) => println!("{}", msg)
+        }
+        return scope;
+    }
+    // Parse tokens to exp
+    let exp: Exp = match parse_in_scope(&mut tokens, scope, variable_scope_map) {
+        Result::Ok(exp) => exp,
+        Result::Err(_) => {
+            println!("Syntax error");
+            return scope
+        }
+    };
+    // Evaluate expression
+    match eval_expression(exp, stack) {
+        Result::Ok(Const::None) => (),
+        Result::Ok(val) => {
+            println!("{}", val);
+        },
+        Result::Err(err) => println!("Error: {}", err.msg)
+    };
+    scope
+}
+
+/**
+ * We need to handle let expression in a different way when
+ * in interactive mode
+ */
+fn eval_let(
+    tokens: &mut Vec<Token>,
+    stack: &mut Vec<Const>,
+    scope: usize,
+    variable_scope_map: &mut HashMap<String, usize>
+) -> Result<usize, String> {
+    // Pop let token
+    match tokens.pop() {
+        Option::Some(Token::Let) => (),
+        _ => panic!("First token must be a let when calling handle_let function")
+    };
+    // Pop variable token
+    let var_name = match tokens.pop() {
+        Option::Some(Token::Operand(Operand::Var(name))) => name,
+        _ => return Result::Err(String::from("SyntaxError: expected variable token"))
+    };
+
+    // Pop "=" token
+    match tokens.pop() {
+        Option::Some(Token::Operator(Operator::Assign)) => (),
+        _ => return Result::Err(String::from("SyntaxError: expected '=' token"))
+    };
+
+    let exp: Exp = match parse_in_scope(tokens, scope, variable_scope_map) {
+        Result::Ok(exp) => exp,
+        Result::Err(err) => return Result::Err(String::from("SyntaxError"))
+    };
+    let val = match eval_expression(exp, stack) {
+        Result::Ok(val) => val,
+        Result::Err(err) => return Result::Err(err.msg)
+    };
+    variable_scope_map.insert(var_name, scope);
+    stack.push(val);
+    Result::Ok(scope + 1)
+}
