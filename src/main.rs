4836f7542a692156f0d888be737bd2be8d0138ba
epieffe.eth <ferrariepifanio@gmail.com>
2023-01-15 01:27:38 +0100

    Run programs from file

--- src/main.rs
+++ src/main.rs
@@ -5,129 +5,21 @@ mod lexer;
 mod semantics;
 mod shell;
 mod value;
+mod run;
 
-use std::ops::Add;
+use std::env;
+use run::run_file;
 
-use parser::parse;
-
-use lexer::tokenize;
-
-use token::Token;
-
-use expression::{Const, Var};
-use expression::Exp;
-
-use semantics::eval;
-use value::{Value};
 
 fn main() {
-    let mode = "SHELL";
-
-    if mode == "SHELL" {
-        shell::run_shell()
+    let mut args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        let file_path: String = args.remove(1);
+        run_file(file_path)
     } else {
-        run_text()
+        shell::run_shell()
     }
 }
-
-fn run_text() {
-    let mut stack: Vec<Const> = Vec::new();
-    let scope: usize = 0;
-
-    let text = "let x = 1 ; x + 1";
-
-    //let text = "let f = fn ( x , y ) { x + y } ; f ( 3 , 2 ) ; f ( 2 , 3 )";
-
-    let text = "{ let f = fn ( x ) { x + 2 } ; let y = 4 ; f ( 1 ) ; f ( f ( y ) ) }";
-
-    // let text = "let f = fn ( x , y ) { x + y } ; f ( 1 , f ( 5 , 6 ) )";
-
-    //let text = "let x = 0 ; let y = 0 ; if ( x == 0 ) { y = 1 } else { y = 2 } ; y";
-
-    // let text = String::from("if true {
-    //     let x = 3 ;
-    //     {
-    //         let y = 3 ;
-    //         y = 4 ;
-    //         x ( y , 3 , y ( 4 ) , 4 + 4 )
-    //     }
-    //     4 ;
-    //     x + 3 ;
-    //     ! ! ( x == 3 )
-    // } else { 4 }");
-
-    let text = String::from(text);
-    let mut tokens: Vec<Token> = tokenize(text).unwrap_or_else(|err| {
-        panic!("TokenizerError")
-    });
-
-    // Parse tokens to exp
-    let exp: Exp = parse(&mut tokens).unwrap_or_else(|err| {
-        panic!("ParserError: {}", err.msg)
-    });
-
-    println!("{}", exp_to_string(&exp));
-    println!("########");
-
-    // Evaluate expression
-    let val = eval(&exp).unwrap_or_else(|err| {
-        panic!("RuntimeError: {}", err.msg)
-    });
-
-    println!("Result: {}", val);
-
-}
-
-fn const_to_string(c: &Const) -> String {
-    match c {
-        Const::Integer(i) => i.to_string(),
-        Const::Boolean(b) => b.to_string(),
-        Const::String(s) => format!("\"{}\"", s),
-        Const::None => String::from("None")
-    }
-}
-
-fn var_to_string(var: &Var) -> String {
-    format!("{}_{}", var.name, var.scope)
-}
-
-fn vars_to_string(vars: &Vec<Var>) -> String {
-    let names: Vec<String> = vars.iter().map(|var| {var_to_string(var)}).collect();
-    format!("{}", names.join(", "))
-}
-
-pub fn exp_to_string(exp: &Exp) -> String {
-    match exp {
-        Exp::Const(c) => const_to_string(c),
-        Exp::Var(x) => var_to_string(x),
-        Exp::Decl(x, val, scope) => format!("let {} = {};\n{}", var_to_string(x), exp_to_string(val), exp_to_string(scope)),
-        Exp::Function(args, body) => format!("fn ({}){{\n{}\n}}", vars_to_string(args), exp_to_string(body)),
-        Exp::Assign(x, e) => format!("{} = {}", var_to_string(x), exp_to_string(e)),
-        Exp::Seq(e1, e2) => format!("{};\n{}", exp_to_string(e1), exp_to_string(e2)),
-        Exp::Sum(e1, e2) => format!("{} + {}", exp_to_string(e1), exp_to_string(e2)),
-        Exp::Sub(e1, e2) => format!("{} - {}", exp_to_string(e1), exp_to_string(e2)),
-        Exp::Mul(e1, e2) => format!("{} * {}", exp_to_string(e1), exp_to_string(e2)),
-        Exp::Div(e1, e2) => format!("{} / {}", exp_to_string(e1), exp_to_string(e2)),
-        Exp::Lt(e1, e2) => format!("{} < {}", exp_to_string(e1), exp_to_string(e2)),
-        Exp::Gt(e1, e2) => format!("{} > {}", exp_to_string(e1), exp_to_string(e2)),
-        Exp::Eq(e1, e2) => format!("{} == {}", exp_to_string(e1), exp_to_string(e2)),
-        Exp::Neq(e1, e2) => format!("{} != {}", exp_to_string(e1), exp_to_string(e2)),
-        Exp::And(e1, e2) => format!("{} && {}", exp_to_string(e1), exp_to_string(e2)),
-        Exp::Or(e1, e2) => format!("{} || {}", exp_to_string(e1), exp_to_string(e2)),
-        Exp::Not(e) => format!("!{}", exp_to_string(e)),
-        Exp::IfThenElse(e, e1, e2) => format!("if {} {{ {} }} else {{ {} }}", exp_to_string(e), exp_to_string(e1), exp_to_string(e2)),
-        Exp::FunctionCall(e, args) => format!("{}({})", exp_to_string(e), args_to_string(args))
-    }
-}
-
-fn args_to_string(args: &Vec<Exp>) -> String {
-    args.iter().map(|exp| {exp_to_string(exp)}).reduce(|mut a, b| {
-        a.push_str(&format!(", {}", b));
-        return a;
-    })
-    .unwrap_or(String::from(" "))
-}
-
 
 // ###################################### TEST ###########################################################
 
@@ -135,9 +27,10 @@ fn args_to_string(args: &Vec<Exp>) -> String {
 mod tests {
     use parser::parse;
     use semantics::eval;
-    use value::{V};
-    use std::collections::HashMap;
-    use crate::expression::Var;
+    use value::{V, Value};
+    use expression::Exp;
+    use token::Token;
+    use lexer::tokenize;
 
     // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
@@ -149,7 +42,6 @@ mod tests {
         let exp: Exp = parse(&mut tokens).or(Result::Err(()))?;
     
         // Evaluate expression
-        let mut stack: Vec<Const> = Vec::new();
         let val = eval(&exp).or(Result::Err(()))?;
         Result::Ok(val)
     }
